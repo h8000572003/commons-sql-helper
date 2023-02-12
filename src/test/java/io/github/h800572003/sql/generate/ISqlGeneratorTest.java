fb837da9017ae3ba93b2e89914aa84af0cebac4f
andytsai <wa.addi@gmail.com>
2023-02-12 23:23:49 +0800

    0.6.6

--- src/test/java/io/github/h800572003/sql/generate/ISqlGeneratorTest.java
+++ src/test/java/io/github/h800572003/sql/generate/ISqlGeneratorTest.java
@@ -22,10 +22,11 @@ class ISqlGeneratorTest {
         ;
         log.info("code:{}", code);
 
-        Assertions.assertTrue(code.contains("ISql sql = SqlBuilder.body().add(\" select \").add(\" c1,c2,c3 \").add(\" from \").add(\" table \");"));
+        Assertions.assertTrue(code.contains("ISql sql = SqlBuilder.body().addSpace(Selects.SELECT.name()).addSpace(\"c1,c2,c3\").addSpace(Selects.FROM.name()).addSpace(\"table\");"));
 
 
     }
+
 
 
     @Test
@@ -36,20 +37,13 @@ class ISqlGeneratorTest {
 
         log.info("code:{}", code);
 
-        Assertions.assertTrue(code.contains("ISql sql = SqlBuilder.body().add(\" select \").add(\" c1,c2,c3 \").add(\" from \").add(\" table \").add(\" where \").add(\" a=:a \").add(\" and \").add(\" b=:b \").add(\" and \").add(\" c=:c \");"));
+        Assertions.assertTrue(code.contains(" ISql sql = SqlBuilder.body().addSpace(Selects.SELECT.name()).addSpace(\"c1,c2,c3\").addSpace(Selects.FROM.name()).addSpace(\"table\").addSpace(Selects.WHERE.name()).addSpace(\"a=:a\").addSpace(Selects.AND.name()).addSpace(\"b=:b\").addSpace(Selects.AND.name()).addSpace(\"c=:c\")"));
 
 
 
     }
-    @Test
-    void test(){
-        StringSqlGeneratorPrint.print("select * from c1,c2 where a='b' \n \t \r and d =: d");
-        System.out.print(this.sql());
-    }
-    public ISql sql() {
-        ISql sql = SqlBuilder.body().addSpace(Selects.SELECT.name()).addSpace("*").addSpace(Selects.FROM.name()).addSpace("c1,c2").addSpace(Selects.WHERE.name()).addSpace("a='b'").addSpace(Selects.AND.name()).addSpace("d").addSpace("=:").addSpace("d");
-   return sql;
-    }
+
+
 
 
     public static void main(String[] args) {
